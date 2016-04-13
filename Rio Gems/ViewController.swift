387c3a6412d9217aecd32a5a6273c1959e32812a
João Gabriel Carvalho <jgcarvalho93@gmail.com>
2016-04-13 14:39:33 -0300

    Initial Commit

---
+++ Rio Gems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rio Gems
+//
+//  Created by Joao Gabriel Carvalho on 13/04/16.
+//  Copyright © 2016 Joao Gabriel Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
